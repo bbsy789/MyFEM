51f680a740765d39c129b6c346af20eb9fd8ec77
bbsy789 <bbsy789@126.com>
2021-10-20 14:41:08 +0800

    update

--- include/base.h
+++ include/base.h
@@ -2,7 +2,6 @@
 #define _BASE_H
 //基本数据的定义
 
-
 #define _IN
 #define _OUT
 #define _IN_OUT
@@ -13,17 +12,6 @@ typedef unsigned int NR;//定义受约束的节点总数
 typedef unsigned int NU;//定义总单元数NU
 typedef unsigned int QQ;//定义总荷载数QQ
 typedef unsigned int KE;//定义单元物理性质数KE
-
-//为了不失一般性，使用3维坐标表示，当遇到平面问题时，某一个坐标轴值即为0。
-typedef struct point
-{
-    unsigned int index;//节点号
-    double X;//X轴坐标
-    double Y;//Y轴坐标
-    double Z;//Z轴坐标
-}POINT;
-
-typedef POINT* PPOINT;//定义指向POINT的指针类型为PPOINT
 
 typedef struct element_attribute
 {
@@ -56,6 +44,5 @@ typedef struct load
     float load_ty;//载荷位置theta_y
     float load_tz;//载荷位置theta_z
 }LOAD;
-
 
  #endif

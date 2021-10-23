1db7e62b5b11bd642254704ed17e7bf5b73946df
bbsy789 <bbsy789@126.com>
2021-10-23 20:17:35 +0800

    ipad submit!

--- src/pre.cpp
+++ src/pre.cpp
@@ -6,37 +6,48 @@ namespace pre
     
     ELEMENT_ATTRIBUTE* Input_E_A(_IN double A,_IN float E,_IN float G,_IN double I,_IN double L,_OUT ERROR_ID* errorID,_OUT pre::STACKS* S)
     {
-        pre::STACKS S;
-        ELEMENT_ATTRIBUTE* ptr = NULL;//定义一个结构体指针，初始化为NULL
-
+        ELEMENT_ATTRIBUTE* element_attribute = NULL;//定义一个结构体指针，初始化为NULL
+        ELEMENT_ATTRIBUTE_NODE* element_attribute_node = NULL;
+        
         if(errorID == NULL)
         {
             return NULL;
         }
-
-        ptr = (ELEMENT_ATTRIBUTE*)malloc(sizeof(ELEMENT_ATTRIBUTE));//分配内存空间，注意释放
-        if(!ptr)
+        
+        *errorID = _ERROR_NO_ERROR;
+        
+        if(A<=0）
         {
-            //赋值
-            ptr->A = A;
-            ptr->E = E;
-            ptr->G = G;
-            ptr->I = I;
-            ptr->L = L;
-            *errorID = _ERROR_NO_ERROR	;
-
-            return ptr;
-        }
-        else
-        {
-            free(ptr);
-            ptr == NULL;
-            *errorID = _ERROR_FAILED_TO_ALLOCATE_HEAP_MEMORY;
+            *errorID = _ERROR_INPUT_PARAMETERS_ERROR;
             return NULL;
         }
+
+        element_attribute = (ELEMENT_ATTRIBUTE*)malloc(sizeof(ELEMENT_ATTRIBUTE));//分配内存空间，注意释放
+        element_attribute_node = (ELEMENT_ATTRIBUTE_NODE*)malloc(sizeof(ELEMENT_ATTRIBUTE_NODE));
+        if(element_attribute == NULL || element_attribute_node == NULL)
+        {
+            free(element_attribute);
+            element_attribute == NULL;
+            free(element_attribute_node);
+            matrixNode = NULL;
+
+            *errorID = _ERROR_FAILED_TO_ALLOCATE_HEAP_MEMORY;
+            return NULL;
+        }    
+        //赋值
+        element_attribute->A = A;
+        element_attribute->E = E;
+        element_attribute->G = G;
+        element_attribute->I = I;
+        element_attribute->L = L；
+        element_attribute_node->data = element_attribute;
+        element_attribute_node->next = S->element_attribute_node;
+        S->element_attribute_node = element_attribute_node;
+
+        return element_attribute;
     }
 
-    MATRIX* Compute_PBES_NS(ELEMENT_ATTRIBUTE* ptr)
+    MATRIX* Compute_PBES_NS(ELEMENT_ATTRIBUTE* element_attribute)
     {
         
     }
